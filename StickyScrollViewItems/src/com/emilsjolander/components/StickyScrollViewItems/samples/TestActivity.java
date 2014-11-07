5519871b90469185552bf394420ccfba3cc377d0
Binil Surendran <binil1931@gmail.com>
2014-11-07 15:18:04 +0530

    Updated code

--- StickyScrollViewItems/src/com/emilsjolander/components/StickyScrollViewItems/samples/TestActivity.java
+++ StickyScrollViewItems/src/com/emilsjolander/components/StickyScrollViewItems/samples/TestActivity.java
@@ -14,22 +14,22 @@ import android.widget.Toast;
 
 import com.emilsjolander.components.StickyScrollViewItems.R;
 
- public class TestActivity extends Activity {
-    /** Called when the activity is first created. */
-   
-	 ImageView img_note;
-	 ImageView img_person;
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-      
-        img_note = (ImageView)findViewById(R.id.mybuttonNote);
-        img_person = (ImageView)findViewById(R.id.mybuttonPerson);
-        		
-        findViewById(R.id.drag).setBackgroundDrawable(getResources().getDrawable(R.drawable.android_background));
-        
-        findViewById(R.id.drag).setOnTouchListener(new OnTouchListener() {
-			
+public class TestActivity extends Activity {
+	/** Called when the activity is first created. */
+
+	ImageView img_note;
+	ImageView img_person;
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		img_note = (ImageView)findViewById(R.id.mybuttonNote);
+		img_person = (ImageView)findViewById(R.id.mybuttonPerson);
+
+		findViewById(R.id.drag).setBackgroundDrawable(getResources().getDrawable(R.drawable.android_background));
+
+		findViewById(R.id.drag).setOnTouchListener(new OnTouchListener() {
+
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
 				// TODO Auto-generated method stub
@@ -37,47 +37,47 @@ import com.emilsjolander.components.StickyScrollViewItems.R;
 				return false;
 			}
 		});
-        
-        findViewById(R.id.mybuttonNote).setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				
-				 findViewById(R.id.layout_note).setVisibility(View.VISIBLE);
-				 findViewById(R.id.layout_image_person).setVisibility(View.GONE);
-				 
-				
-			}
-		});
- findViewById(R.id.mybuttonPerson).setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				
-				 findViewById(R.id.layout_note).setVisibility(View.GONE);
-				 findViewById(R.id.layout_image_person).setVisibility(View.VISIBLE);
-				 
-				
-			}
-		});
- 
-    }
-    
-   public void note(View v)
-    {
-	   img_note.setImageResource(R.drawable.book_with_arrow);
-    	 img_person.setImageResource(R.drawable.man_vector_active);
-		 findViewById(R.id.layout_note).setVisibility(View.VISIBLE);
-		 findViewById(R.id.layout_image_person).setVisibility(View.GONE);
-    }
-    
-    public void person(View v)
-    {
- 	   img_note.setImageResource(R.drawable.book_icon_active);
-  	 img_person.setImageResource(R.drawable.tab_person_with_arrow);
-		 findViewById(R.id.layout_note).setVisibility(View.GONE);
-		 findViewById(R.id.layout_image_person).setVisibility(View.VISIBLE);
-    }
+
+//		findViewById(R.id.mybuttonNote).setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View arg0) {
+//				// TODO Auto-generated method stub
+//
+//				findViewById(R.id.layout_note).setVisibility(View.VISIBLE);
+//				findViewById(R.id.layout_image_person).setVisibility(View.GONE);
+//
+//
+//			}
+//		});
+//		findViewById(R.id.mybuttonPerson).setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View arg0) {
+//				// TODO Auto-generated method stub
+//
+//				findViewById(R.id.layout_note).setVisibility(View.GONE);
+//				findViewById(R.id.layout_image_person).setVisibility(View.VISIBLE);
+//
+//
+//			}
+//		});
+
+	}
+
+	public void note(View v)
+	{
+		img_note.setImageResource(R.drawable.book_with_arrow);
+		img_person.setImageResource(R.drawable.man_vector_active);
+		findViewById(R.id.layout_note).setVisibility(View.VISIBLE);
+		findViewById(R.id.layout_image_person).setVisibility(View.GONE);
+	}
+
+	public void person(View v)
+	{
+		img_note.setImageResource(R.drawable.book_icon_active);
+		img_person.setImageResource(R.drawable.tab_person_with_arrow);
+		findViewById(R.id.layout_note).setVisibility(View.GONE);
+		findViewById(R.id.layout_image_person).setVisibility(View.VISIBLE);
+	}
 }
